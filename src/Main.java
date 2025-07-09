c4e82ce6de0cf26fa03c7168755fca81858d6bd3
Shaun Titus Geevarghese <shauntg19@gmail.com>
2025-07-09 15:21:40 +0530

    finished shopping cart project

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,15 @@ public class Main {
         System.out.print("What item would you like to buy: ");
         item = scanner.nextLine();
 
-        System.out.println(item);
+        System.out.print("What is the price: ");
+        price = scanner.nextDouble();
+
+        System.out.print("How many would you like: ");
+        quantity = scanner.nextInt();
+
+        total = price*quantity;
+        System.out.println("\n You have bought "+ quantity +" "+ item+"/s");
+        System.out.println("Your Total is "+ curency+total);
 
 
 
