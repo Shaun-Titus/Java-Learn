193a6500d712e431af200e662efb34ee98e0d706
Shaun Titus Geevarghese <shauntg19@gmail.com>
2025-07-09 14:33:11 +0530

    Add 2 var

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        String item;
+        double price;
+
+
+
+
+        scanner.close();
+
+
+    }
+
+
+}
+
