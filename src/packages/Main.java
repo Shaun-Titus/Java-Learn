f3e70278e147889526bc51fef417482bf980246b
Shaun Titus Geevarghese <shauntg19@gmail.com>
2025-08-14 23:23:45 +0530

    inheritance

---
+++ src/packages/Main.java
@@ -0,0 +1,14 @@
+package packages;
+
+import packages.Demo;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create an instance of the Demo class
+        Demo myDemoObject = new Demo();
+
+        // Access the public member variable and method
+        System.out.println(myDemoObject.message);
+        myDemoObject.displayMessage();
+    }
+}
